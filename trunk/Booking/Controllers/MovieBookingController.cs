d816b0a08d6b82d85cd35c1bb2404cfd1999053a
smoneyan@gmail.com <smoneyan@gmail.com@ae0285c0-fd69-8cfb-be9b-b9452f3e47d9>
2012-05-21 17:20:15 +0000

    MVC Booking

---
+++ trunk/Booking/Controllers/MovieBookingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieBooking.BLL.Entities;
+
+namespace MovieBooking.MVC.UI.Controllers
+{
+    public class MovieBookingController : Controller
+    {
+        //
+        // GET: /MovieBooking/
+
+        public ActionResult Index()
+        {
+         
+            BookingRepository repo = new BookingRepository();
+
+            IEnumerable <Booking> bookings = repo.GetMovieBookings();
+            
+            return View(bookings.ToList());
+        }
+
+    }
+}
