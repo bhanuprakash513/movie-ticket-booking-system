d648cb3d97b5076699a2dc2fdae644b4752f5306
smoneyan@gmail.com <smoneyan@gmail.com@ae0285c0-fd69-8cfb-be9b-b9452f3e47d9>
2012-05-22 18:56:57 +0000

    Updated DAL

--- trunk/DAL/MovieBooking.DAL.Model.Designer.cs
+++ trunk/DAL/MovieBooking.DAL.Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'E:\movie-ticket-booking-system\DAL\MovieBooking.DAL.Model.edmx'. 
+﻿// Default code generation is disabled for model 'C:\subu\Mtech\Dot.Net\movie-ticket-booking-system-new\DAL\MovieBooking.DAL.Model.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
